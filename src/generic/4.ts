82d5805fa2e2bba175cde735ad42db64348e8564
yurii2007 <yurash862@gmail.com>
2023-09-14 15:12:31 +0300

    bugfix

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,17 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
+interface IProps {
+  title: string;
+}
+
 class Component<T> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component<{title: string}> {
+class Page extends Component<IProps> {
   pageInfo () {
     console.log(this.props.title);
   }
